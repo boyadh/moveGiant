898634c25a92bc8980df791e62c7f7da3ccd11af
Boyad <boyadhollingshead@gmail.com>
2020-09-05 17:35:44 -0600

    added nav bar component and nar bar bg

--- imports/components/navBar/navBar.js
+++ imports/components/navBar/navBar.js
@@ -1,18 +1,26 @@
-import angular from 'angular';
+import angular, { element } from 'angular';
 import angularMeteor from 'angular-meteor';
 import template from './navBar.html';
-//import styles from './navBar.css';
+import style from './navBar.css';
+import bgImg from '../../ui/img/nav-bar-bg.jpg';
 
 class NavBarCtrl {
     constructor() {
         this.label = "This is from the constructor in navBar component!";
+        element.css({})
     }
+    
 }
+
+var app = angular.module('background',[]);
+app.controller('background-ctrl', function($scope){});
+
+app.directive('')
 
 export default angular.module('navBar', [
     angularMeteor
 ]).component('navBar', {
     templateUrl: 'imports/components/navBar/navBar.html',
-    stylesUrl: 'imports/components/navBar/navBar.css',
+    styleUrl: 'imports/components/navBar/navBar.css',
     controller: NavBarCtrl
 })
